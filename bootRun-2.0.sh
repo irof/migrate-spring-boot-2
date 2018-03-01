f468d529fdb8f5f22d3ff6ccabaef810e704278c
irof <irof@hogedriven.net>
2018-03-02 00:02:46 +0900

    アプリケーション追加

---
+++ bootRun-2.0.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./gradlew -b build-2.0.gradle clean bootRun
---
+++ src/main/java/hogedriven/HogedrivenApplication.java
@@ -0,0 +1,12 @@
+package hogedriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogedrivenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogedrivenApplication.class, args);
+    }
+}
